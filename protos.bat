c0fdc6d4b63be6d1bb7e44f694473ed79084999e
Cifko <gcifko@gmail.com>
2023-09-28 08:36:48 +0200

    fix naming issues

--- protos.bat
+++ protos.bat
@@ -1,3 +1,3 @@
 pip install grpcio-tools
 mkdir protos
-python -m grpc_tools.protoc --proto_path ../tari/applications/tari_app_grpc/proto/  --python_out=./protos --grpc_python_out=./protos ../tari/applications/tari_app_grpc/proto/*.proto
+python -m grpc_tools.protoc --proto_path ../tari/applications/minotari_app_grpc/proto/  --python_out=./protos --grpc_python_out=./protos ../tari/applications/minotari_app_grpc/proto/*.proto
