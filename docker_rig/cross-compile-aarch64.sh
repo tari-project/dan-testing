bcbf03100527de7781e6c4d53edb39ad086d787d
C.Lee Taylor <leet@tari.com>
2023-10-05 16:27:33 +0200

    GHA - workaround space restraint and fix Debian cross-compile for arm64

--- docker_rig/cross-compile-aarch64.sh
+++ docker_rig/cross-compile-aarch64.sh
@@ -18,3 +18,12 @@ export RUSTFLAGS="-C target_cpu=$ARCH"
 export ROARING_ARCH=$ARCH
 rustup target add aarch64-unknown-linux-gnu
 rustup toolchain install stable-aarch64-unknown-linux-gnu --force-non-host
+
+# Check for Debian
+if [ -f "/etc/debian_version" ] ; then
+  dpkg --add-architecture arm64
+  apt-get update
+  apt-get install -y pkg-config libssl-dev:arm64
+  export AARCH64_UNKNOWN_LINUX_GNU_OPENSSL_INCLUDE_DIR=/usr/include/aarch64-linux-gnu/openssl/
+  export PKG_CONFIG_ALLOW_CROSS=1
+fi
